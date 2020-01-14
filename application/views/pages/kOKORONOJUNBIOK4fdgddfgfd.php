57bf9f108254dbd281f0a8d15c70be98a9b29125
Lea23VC <leandro.villalobos.dx@gmail.com>
2020-01-14 10:23:18 -0300

    AAAAAAAA MEU PIXULINHA

---
+++ application/views/pages/kOKORONOJUNBIOK4fdgddfgfd.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+<h1>kOKORO NO JUNBI OK 4</h1>
+<p> ID del intermediario: gfd</p>
+<p> Producido por: dfgfd</p>
+<p> Faenado en: fdgd </p>
+<p> Desmalezado: ghgf</p>
+<p> Fumigaciones: hjhj</p>
+<p> Aplicaciones de plagas preventivas: hjhg</p>
+<p> Fertilizaciones Organicas: : ghj</p>
+<p> Intermediario(transporte): ghjhgj</p>
+<p> Tipo de agua: ghj</p>
+<p> Profundidad efectiva: hgjgh</p>
+<p> Maquinaria: ghjgh</p>
+
+</body>
+</html> 
